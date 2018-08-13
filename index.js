2afb35b8315ff16bbe18528a2b8e5911e472c436
Ashkir10 <32412593+Ashkir10@users.noreply.github.com>
2018-08-13 23:29:11 +0300

    update listening port to also support cloud hosting: heroku

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ http.createServer(function(req, res) {
     
     
 
-}).listen(1337, "127.0.0.1");
+}).listen(process.env.PORT || 1337, "127.0.0.1");
 
 console.log("Server running at http://127.0.0.1:1337");
